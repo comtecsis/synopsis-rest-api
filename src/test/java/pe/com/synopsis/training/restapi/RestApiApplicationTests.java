d24086e7100a7aa872da2db3f32ae875df16b0ee
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-07-22 13:28:44 -0500

    Commit inicial

---
+++ src/test/java/pe/com/synopsis/training/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.synopsis.training.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
